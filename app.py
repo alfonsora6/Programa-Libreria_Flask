bd1b448b95f18da749848a1d9c53b7f18b10a493
Alfonso Roldán <megaalfonso84@gmail.com>
2022-04-22 18:29:34 +0200

    Fichero app.py añadido

---
+++ app.py
@@ -0,0 +1,26 @@
+from distutils.log import debug
+from flask import Flask, render_template, abort, request
+from funciones import *
+
+app = Flask(__name__)
+
+datos=Leer_Json("books.json")
+
+@app.route('/')
+def inicio():
+    return render_template("index.html",datos=datos)
+
+@app.route('/libro/<isbn>')
+def libro(isbn):
+    try:
+        for libro in datos:
+            if libro.get("isbn") == isbn:
+                return render_template("libro.html", libro=libro)
+    except:
+        return abort(404)
+
+@app.route('/categorias/<categoria>')
+def categorias(categoria):
+    return render_template("categorias.html", datos=datos, categoria=categoria)
+
+app.run("0.0.0.0",5000,debug=True)
